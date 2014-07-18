828c4d38ce5e63fff348020734699c004be5787f
Maha <eneyed@gmail.com>
2014-07-18 16:16:06 +0530

    maha | few changes to deploy script

--- ctrls/deploy.sh
+++ ctrls/deploy.sh
@@ -7,21 +7,25 @@ FILENAME="drishti-forms-migrator-${VERSION}.tar"
 SNAPSHOT_VERSION=`echo $VERSION | cut -d "-" -f1`
 EXTRACTED_DIRECTORY="drishti-forms-migrator-'${SNAPSHOT_VERSION}'-SNAPSHOT/bin"
 
-#copying json-to-xls.yml to Prod
-scp json-to-xls.yml prod://home/motech/config-json-to-xls.yml
+#copying drishti-forms-migrator.yml to Prod && \
+scp drishti-forms-migrator.yml linode:~/config-drishti-forms-migrator.yml && \
 
 echo "FETCHING FILE: ${FILENAME}"
 ssh prod '\
     export time=`date +%F_%T`; \
-    mkdir -p old/backup-drishti-forms-migrator-'${VERSION}'.'${time}' && \
-	pg_dump -U forms_migration_user forms_migration > old/backup-forms-migration-'${VERSION}'.'${time}'/backup-forms-migration-database-'${VERSION}'.'${time}' && \
-    ([[ `ls | grep -q "drishti-forms-migrator.*"; echo $?` = 0 ]] && /bin/mv -f json-to-xls* old/backup-drishti-forms-migrator-'${VERSION}'.'${time}' || true) && \
-    wget http://nexus.motechproject.org/content/repositories/drishti-forms-migrator/app/drishti-forms-migrator/'${SNAPSHOT_VERSION}'-SNAPSHOT/'${FILENAME}' && \
-    tar -xvf '${FILENAME}' && \
-	echo EXTRACTED DIRECTORY '${EXTRACTED_DIRECTORY}' && \
-	cd '${EXTRACTED_DIRECTORY}' && \
-	export JAVA_HOME=/usr/java/default && \
-	echo "Migrating Database"	&& \
-	sh ./json-to-xls db migrate ../../config-json-to-xls.yml && \
-	echo "Starting the service" && \
-	sh ./json-to-xls server ../../config-json-to-xls.yml &'
+    mkdir -p old/backup-drishti-forms-migrator-'${VERSION}'.'${time}';\
+    mkdir -p drishti-forms-migrator;\
+    mv -p ../config-drishti-forms-migrator.yml config-drishti-forms-migrator.yml;\
+    cd drishti-forms-migrator;\
+    pg_dump -U forms_migration_user forms_migration > old/backup-forms-migration-'${VERSION}'.'${time}'/backup-forms-migration-database-'${VERSION}'.'${time}';\
+    ([[ `ls | grep -q "drishti-forms-migrator.*"; echo $?` = 0 ]] && /bin/mv -f drishti-forms-migrator* old/backup-drishti-forms-migrator-'${VERSION}'.'${time}' || true);\
+    wget http://nexus.motechproject.org/content/repositories/drishti-forms-migrator/app/drishti-forms-migrator/'${SNAPSHOT_VERSION}'-SNAPSHOT/'${FILENAME}';\
+    tar -xvf '${FILENAME}';\
+    rm '${FILENAME}';\
+	echo EXTRACTED DIRECTORY '${EXTRACTED_DIRECTORY}';\
+	cd '${EXTRACTED_DIRECTORY}';\
+	export JAVA_HOME=/usr/java/default;\
+	echo "Migrating Database";\
+	sh ./drishti-forms-migrator db migrate ../../config-drishti-forms-migrator.yml;\
+	echo "Starting the service";\
+	sh ./drishti-forms-migrator server ../../config-drishti-forms-migrator.yml &'
--- xls_ona_io/deploy.sh
+++ xls_ona_io/deploy.sh
@@ -7,20 +7,24 @@ FILENAME="drishti-forms-migrator-${VERSION}.tar"
 SNAPSHOT_VERSION=`echo $VERSION | cut -d "-" -f1`
 EXTRACTED_DIRECTORY="drishti-forms-migrator-'${SNAPSHOT_VERSION}'-SNAPSHOT/bin"
 
-#copying drishti-forms-migrator.yml to Prod
-scp drishti-forms-migrator.yml linode://root/config-drishti-forms-migrator.yml
+#copying drishti-forms-migrator.yml to Prod && \
+scp drishti-forms-migrator.yml linode:~/config-drishti-forms-migrator.yml && \
 
 echo "FETCHING FILE: ${FILENAME}"
 ssh xlsonaio '\
     export time=`date +%F_%T`; \
-    mkdir -p old/backup-drishti-forms-migrator-'${VERSION}'.'${time}' && \
-	pg_dump -U forms_migration_user forms_migration > old/backup-forms-migration-'${VERSION}'.'${time}'/backup-forms-migration-database-'${VERSION}'.'${time}' && \
-    ([[ `ls | grep -q "drishti-forms-migrator.*"; echo $?` = 0 ]] && /bin/mv -f json-to-xls* old/backup-drishti-forms-migrator-'${VERSION}'.'${time}' || true) && \
-    wget http://nexus.motechproject.org/content/repositories/drishti-forms-migrator/app/drishti-forms-migrator/'${SNAPSHOT_VERSION}'-SNAPSHOT/'${FILENAME}' && \
-    tar -xvf '${FILENAME}' && \
-	echo EXTRACTED DIRECTORY '${EXTRACTED_DIRECTORY}' && \
-	cd '${EXTRACTED_DIRECTORY}' && \
-	echo "Migrating Database"	&& \
-	sh ./drishti-content-migrator db migrate ../../config-drishti-forms-migrator.yml && \
-	echo "Starting the service" && \
-	sh ./drishti-content-migrator server ../../config-drishti-forms-migrator.yml &'
+    mkdir -p old/backup-drishti-forms-migrator-'${VERSION}'.'${time}';\
+    mkdir -p drishti-forms-migrator;\
+    mv -p ../config-drishti-forms-migrator.yml config-drishti-forms-migrator.yml;\
+    cd drishti-forms-migrator;\
+    pg_dump -U forms_migration_user forms_migration > old/backup-forms-migration-'${VERSION}'.'${time}'/backup-forms-migration-database-'${VERSION}'.'${time}';\
+    ([[ `ls | grep -q "drishti-forms-migrator.*"; echo $?` = 0 ]] && /bin/mv -f drishti-forms-migrator* old/backup-drishti-forms-migrator-'${VERSION}'.'${time}' || true);\
+    wget http://nexus.motechproject.org/content/repositories/drishti-forms-migrator/app/drishti-forms-migrator/'${SNAPSHOT_VERSION}'-SNAPSHOT/'${FILENAME}';\
+    tar -xvf '${FILENAME}';\
+    rm '${FILENAME}';\
+	echo EXTRACTED DIRECTORY '${EXTRACTED_DIRECTORY}';\
+	cd '${EXTRACTED_DIRECTORY}';\
+	echo "Migrating Database";\
+	sh ./drishti-forms-migrator db migrate ../../config-drishti-forms-migrator.yml;\
+	echo "Starting the service";\
+	sh ./drishti-forms-migrator server ../../config-drishti-forms-migrator.yml &'
